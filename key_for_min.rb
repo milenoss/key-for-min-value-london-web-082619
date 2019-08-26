e3e646351ef0e693b9f96154a890c398ac9a3464
Milen Khanal <milenkhanal@gmail.com>
2019-08-26 20:39:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,12 +6,9 @@ def key_for_min_value(name_hash)
   new_hash = {}
 name_hash.each do |key,value| 
    value 
-   if value == new_hash
-     nil 
-   else
-   new_hash = name_hash.sort_by {|k, v| k}
-       name_hash[0][0]
-  end
   end
 end
 
+def key_for_min_value(test)
+  if test == {}
+    test.sort_
