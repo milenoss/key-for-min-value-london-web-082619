b87d6d07fc348ba281c7d369722b58ee2cd1a61b
Milen Khanal <milenkhanal@gmail.com>
2019-08-26 21:00:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,11 @@ require 'pry'
 def key_for_min_value(name_hash)
 if name_hash == {}
   return nil 
-elsif
 name_hash.each do |key,value| 
     value
+    if value < 
     end
   end
 end
 
 
-it "returns the key of the smallest hash value" do
-    expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
-  end
