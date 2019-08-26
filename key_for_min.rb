e1e7c47c0a1e83f861ded8d332b6acb174169751
Milen Khanal <milenkhanal@gmail.com>
2019-08-26 21:27:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,17 @@ def key_for_min_value(name_hash)
 if name_hash == {}
   return nil 
 else
+  min = nil 
 name_hash.each do |key,value| 
+  if min == nil && min > value
+    min = value
+  
    result = value
-  if result 
+  if result < value
+    return key[value]
    
-  end
+        end
+      end
   end
 end
 
